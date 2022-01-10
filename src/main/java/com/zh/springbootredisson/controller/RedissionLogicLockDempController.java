ad77a22fc55c72bd319595e6f80e7efd27a74778
Andy <winglive@163.com>
2022-01-11 02:27:37 +0800

    fix  修复删除其他线程锁的逻辑

--- src/main/java/com/zh/springbootredisson/controller/RedissionLogicLockDempController.java
+++ src/main/java/com/zh/springbootredisson/controller/RedissionLogicLockDempController.java
@@ -10,11 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * 7
@@ -43,9 +42,9 @@ public class RedissionLogicLockDempController {
      * 1.2 祖先有移动move 锁，移动 不允许新增 返回异常信息  5_move_6  ：节点5 移动到 6
      */
     @GetMapping("add")
-    @ApiOperation(value = "文件新增 入参 父节点id ", notes = "测试redis写入")
+    @ApiOperation(value = "1.文件新增", notes = "测试redis写入")
     @ResponseBody
-    public String add(@RequestParam(defaultValue = "001") String userID, String id) {
+    public String add(@RequestParam(defaultValue = "001") String userID, String id, String fileId) {
 
         //0  查看祖先路径有没有删除锁  移动锁 出入
 
@@ -56,38 +55,49 @@ public class RedissionLogicLockDempController {
         lockcheck.add("move_" + id);  //移入
         lockcheck.add("move_" + "5");
         lockcheck.add("move_" + "7");
-        lockcheck.add(id+"_move");  //移除
-        lockcheck.add("5"+"_move");
-        lockcheck.add("7"+"_move");
+        lockcheck.add(id + "_move");  //移除
+        lockcheck.add("5" + "_move");
+        lockcheck.add("7" + "_move");
 
         RSet<String> set = redissonClient.getSet(userID);
-        Boolean s = set.retainAll(lockcheck);
-        if (set.size() > 0) {
-            System.out.println("发现并发锁");
-            return "发现锁： "+set.stream().findFirst().get();
+        Set<String> setCopy=new HashSet<>(set.size());
+        setCopy.addAll(set);
+        List<String> locks1=new ArrayList<>();
+        for(String lockString: lockcheck){
+         locks1 = setCopy.stream().filter(o->o.startsWith(lockString)).collect(Collectors.toList());
+        }
+
+        if(locks1.size()>0){
+            System.out.println("发现并发锁1");
+            return  "发现锁： "+ setCopy.stream().findFirst().get();
+        }
+        setCopy.retainAll(lockcheck);
+        if (setCopy.size() > 0) {
+            System.out.println("发现并发锁2");
+            // todo 不一定是第一个
+            return "发现锁： " + setCopy.stream().findFirst().get();
         } else {
             List<String> addLock = new ArrayList<>();
-            addLock.add("add_" + id);
-            addLock.add("add_" + "5");
-            addLock.add("add_" + "7");
+            addLock.add("add_" + id + "_" + fileId);
+            addLock.add("add_" + "5_" + fileId);
+            addLock.add("add_" + "7_" + fileId);
             try {
                 //2.查询所有父节点id  从ES差  某个节点的信息 祖先节点List  去除0
                 set.addAll(addLock);
                 set.expire(60, TimeUnit.SECONDS);
                 set.forEach(System.out::println);
                 System.out.println("文件正在写入");
-
-                Thread.sleep(30000);
+                Thread.sleep(20000);
 
                 set.forEach(System.out::println);
             } catch (Exception e) {
                 Boolean unLock = set.removeAll(addLock);
                 e.printStackTrace();
-            }finally {
+            } finally {
                 System.out.println("释放锁");
                 Boolean unLock = set.removeAll(addLock);
             }
-            return "用户： " + userID + "文件上传、文件夹新建 " + "结果：  成功" ;
+            return "用户： " + userID + "文件上传、文件夹新建 " + "结果：  成功";
         }
     }
 
@@ -99,23 +109,35 @@ public class RedissionLogicLockDempController {
      * 1.2
      */
     @GetMapping("delete")
-    @ApiOperation(value = "文件删除 传入删除节点 ", notes = "节点删除")
+    @ApiOperation(value = "2.文件删除 ", notes = "节点删除")
     @ResponseBody
-    public String delete(@RequestParam(defaultValue = "001") String userID, @RequestParam(defaultValue = "5")String id) {
+    public String delete(@RequestParam(defaultValue = "001") String userID, @RequestParam(defaultValue = "5") String id) {
 
         //1.查询所有父节点id  从ES获取  某个节点的信息 祖先节点List  去除0
         List<String> deleteLockCheck = new ArrayList<>();
-        deleteLockCheck.add("add_" + id);
         deleteLockCheck.add(id + "_move");
         deleteLockCheck.add("7" + "_move");
         deleteLockCheck.add("move_" + id);
 
-
+        List<String> sourceAddLockCheck = new ArrayList<>();
+        sourceAddLockCheck.add("add_" + id);
         RSet<String> set = redissonClient.getSet(userID);
-        Boolean s = set.retainAll(deleteLockCheck);
-        if (set.size() > 0) {
+        Set<String> setCopy=new HashSet<>();
+        setCopy.addAll(set);
+
+        Boolean add=false;
+        for (String addLock:sourceAddLockCheck) {
+            add = setCopy.stream().anyMatch(o -> o.startsWith(addLock));
+            System.out.println(add);
+            if(add){
+                System.out.println("发现并发 写锁");
+                return "发现锁 1：" + addLock;
+            }
+        }
+        Boolean s = setCopy.retainAll(deleteLockCheck);
+        if (setCopy.size() > 0) {
             System.out.println("发现并发锁");
-            return "发现锁："+set.stream().findFirst().get();
+            return "发现锁 2：" + setCopy.stream().findFirst().get();
         } else {
             // 加删除锁
             // 获取被删除节点的 祖先路径  5 、7
@@ -134,7 +156,7 @@ public class RedissionLogicLockDempController {
             } catch (Exception e) {
                 e.printStackTrace();
                 set.removeAll(deleteLock);
-            }finally {
+            } finally {
                 set.removeAll(deleteLock);
             }
 
@@ -148,22 +170,22 @@ public class RedissionLogicLockDempController {
      * 1.1 判断 6节点上是否有新增锁
      * 1.2 判断 6 节点上是否有移动锁  7 是否移动
      * 1.3 判断 6 节点上有没有删除锁  7节点是否删除
-     *
-     *
+     * <p>
+     * <p>
      * 2.1 判断5 节点是否删除  7 节点是否删除
      * 2.2 判断 5 节点是被否移动 7 节点是否被移动
-     *
      */
     @GetMapping("move")
-    @ApiOperation(value = "文件 ", notes = "节点删除")
+    @ApiOperation(value = "3.文件移动 ", notes = "节点删除")
     @ResponseBody
-    public String move(@RequestParam(defaultValue = "001") String userID,@RequestParam(defaultValue = "6") String id_source,@RequestParam(defaultValue = "5")String id_dest) {
+    public String move(@RequestParam(defaultValue = "001") String userID, @RequestParam(defaultValue = "6") String id_source, @RequestParam(defaultValue = "5") String id_dest) {
 
         //1.先处理 源 6 节点 目标查询所有父节点id  从ES获取
 
 
         List<String> sourceLockCheck = new ArrayList<>();
-        sourceLockCheck.add("add_" + id_source);
+        List<String> sourceAddLockCheck = new ArrayList<>();
+        sourceAddLockCheck.add("add_" + id_source);
         sourceLockCheck.add(id_source + "_move");
         sourceLockCheck.add("7" + "_move");
         sourceLockCheck.add("delete_" + id_source);
@@ -172,11 +194,21 @@ public class RedissionLogicLockDempController {
 
         RSet<String> set = redissonClient.getSet(userID);
         Set<String> set2 = new HashSet<>(set.size());
+        Set<String> setCopy=new HashSet<>(set.size());
+        setCopy.addAll(set);
         set2.addAll(set);
-        Boolean s = set.retainAll(sourceLockCheck);
-        if (set.size() > 0) {
+        Boolean s = setCopy.retainAll(sourceLockCheck);
+        boolean add=false;
+        for (String addLock:sourceAddLockCheck) {
+             add = setCopy.stream().anyMatch(o -> o.startsWith(addLock));
+              if(add){
+                  System.out.println("发现并发 写锁");
+                  return "发现锁：" + addLock;
+              }
+        }
+        if (setCopy.size() > 0) {
             System.out.println("发现并发锁");
-            return "发现锁："+set.stream().findFirst().get();
+            return "发现锁：" + setCopy.stream().findFirst().get();
         } else {
             // 加删除锁
             // 获取被删除节点的 祖先路径  5 、7
@@ -185,14 +217,14 @@ public class RedissionLogicLockDempController {
             //目标锁
             List<String> dest_lock = new ArrayList<>();
             source_lock.add(id_source + "_move");
-            source_lock.add("add_" + "7");
+            source_lock.add("add_" + "7_"+ id_source);
             try {
                 set.addAll(source_lock);
                 set.expire(300, TimeUnit.SECONDS);
                 // 源节点加锁完成
                 System.out.println("源节点加锁完成");
                 // 处理目标节点 5
-                System.out.println(" 处理目标节点"+id_dest);
+                System.out.println(" 处理目标节点" + id_dest);
 
                 List<String> lockcheck = new ArrayList<>();
                 lockcheck.add("delete_" + id_dest);
@@ -202,21 +234,22 @@ public class RedissionLogicLockDempController {
                 Boolean dest = set2.retainAll(lockcheck);
                 if (set2.size() > 0) {
                     System.out.println("发现并发锁");
-                    throw  new Exception("目标加锁失败");
-                }else{
+                    throw new Exception("目标加锁失败");
+                } else {
 
                     dest_lock.add("move_" + id_dest);
-                    dest_lock.add("add_"+ "7");
+                    dest_lock.add("add_" + "7_"+id_dest);
                     set.addAll(dest_lock);
                     System.out.println("目标加锁成功 ..开始移动逻辑");
-                    Thread.sleep(300000);
+                    Thread.sleep(20000);
+                    System.out.println("目标加锁成功 移动逻辑结束");
                 }
 
             } catch (Exception e) {
                 e.printStackTrace();
                 set.removeAll(source_lock);
                 set.removeAll(dest_lock);
-            }finally {
+            } finally {
                 set.removeAll(source_lock);
                 set.removeAll(dest_lock);
             }
@@ -231,22 +264,22 @@ public class RedissionLogicLockDempController {
      * 1.1 判断 6节点上是否有新增锁
      * 1.2 判断 6 节点上是否有移动锁  7 是否移动
      * 1.3 判断 6 节点上有没有删除锁  7节点是否删除
-     *
-     *
+     * <p>
+     * <p>
      * 2.1 判断5 节点是否删除  7 节点是否删除
      * 2.2 判断 5 节点是被否移动 7 节点是否被移动
-     *
      */
     @GetMapping("copy")
-    @ApiOperation(value = "文件 ", notes = "节点删除")
+    @ApiOperation(value = "4.文件复制 ", notes = "文件复制")
     @ResponseBody
-    public String copy(@RequestParam(defaultValue = "001") String userID,@RequestParam(defaultValue = "6") String id_source,@RequestParam(defaultValue = "5")String id_dest) {
+    public String copy(@RequestParam(defaultValue = "001") String userID, @RequestParam(defaultValue = "6") String id_source, @RequestParam(defaultValue = "5") String id_dest) {
 
         //1.先处理 源 6 节点 目标查询所有父节点id  从ES获取
 
 
         List<String> parents = new ArrayList<>();
-        parents.add("add_" + id_source);
+        List<String> sourceAddLockCheck = new ArrayList<>();
+        sourceAddLockCheck.add("add_" + id_source);
         parents.add(id_source + "_move");
         parents.add("7" + "_move");
         parents.add("delete_" + id_source);
@@ -254,26 +287,36 @@ public class RedissionLogicLockDempController {
 
 
         RSet<String> set = redissonClient.getSet(userID);
+        Set<String> setCopy=new HashSet<>(set.size());
+        setCopy.addAll(set);
         Set<String> set2 = new HashSet<>(set.size());
         set2.addAll(set);
-        Boolean s = set.retainAll(parents);
-        if (set.size() > 0) {
+        Boolean s = setCopy.retainAll(parents);
+        boolean add=false;
+        for (String addLock:sourceAddLockCheck) {
+            add = setCopy.stream().anyMatch(o -> o.startsWith(addLock));
+            if(add){
+                System.out.println("发现并发 写锁");
+                return "发现锁：" + addLock;
+            }
+        }
+        if (setCopy.size() > 0) {
             System.out.println("发现并发锁");
-            return "发现锁："+set.stream().findFirst().get();
+            return "发现锁：" + setCopy.stream().findFirst().get();
         } else {
             // 加删除锁
             // 获取被删除节点的 祖先路径  5 、7
             List<String> lock = new ArrayList<>();
             List<String> parents2 = new ArrayList<>();
             parents2.add(id_source + "_move");
-            parents2.add("add_" + "7");
+            parents2.add("add_" + "7_"+id_source);
             try {
                 set.addAll(parents2);
                 set.expire(300, TimeUnit.SECONDS);
                 // 源节点加锁完成
                 System.out.println("源节点加锁完成");
                 // 处理目标节点 5
-                System.out.println(" 处理目标节点"+id_dest);
+                System.out.println(" 处理目标节点" + id_dest);
 
                 List<String> lockcheck = new ArrayList<>();
                 lockcheck.add("delete_" + id_dest);
@@ -283,21 +326,21 @@ public class RedissionLogicLockDempController {
                 Boolean dest = set2.retainAll(lockcheck);
                 if (set2.size() > 0) {
                     System.out.println("发现并发锁");
-                    throw  new Exception("目标加锁失败");
-                }else{
+                    throw new Exception("目标加锁失败");
+                } else {
 
                     lock.add("move_" + id_dest);
-                    lock.add("add_"+ "7");
+                    lock.add("add_" + "7_"+id_dest);
                     set.addAll(lock);
                     System.out.println("目标加锁成功 ..开始移动逻辑");
-                    Thread.sleep(300000);
+                    Thread.sleep(30000);
                 }
 
             } catch (Exception e) {
                 e.printStackTrace();
                 set.removeAll(parents2);
                 set.removeAll(lock);
-            }finally {
+            } finally {
                 set.removeAll(parents2);
                 set.removeAll(lock);
             }
@@ -305,8 +348,6 @@ public class RedissionLogicLockDempController {
         }
         return "用户： " + userID + "文件夹移动成功";
     }
-
-
 
 
 }
